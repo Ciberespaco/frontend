7a6fd06371957812ba9ae2c61baa76c41e2e9e45
Matheus Alves <matheus.alvsbarroso@gmail.com>
2025-05-19 01:58:32 -0300

    chore:added .env

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,28 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
+  compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
+  css: ["~/assets/css/tailwind.css"],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  ssr: false,
+
+  runtimeConfig: {
+    public: {
+      backendURL: process.env.BACKEND_URL,
+    },
+  },
 
   modules: [
-    '@nuxt/image',
-    '@nuxt/icon',
-    '@nuxt/fonts',
-    '@nuxt/eslint'
-  ]
-})
+    "@nuxt/image",
+    "@nuxt/icon",
+    "@nuxt/fonts",
+    "@nuxt/eslint",
+    "shadcn-nuxt",
+    "@pinia/nuxt",
+    "@nuxtjs/google-fonts",
+  ],
+});
